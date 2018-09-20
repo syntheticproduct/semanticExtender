08f2af5ae54ddd6f5693f136ea3d2c0ab7b12a76
Elliot Kim <elliotkim@Elliots-MacBook-Pro.local>
2018-09-20 09:14:30 -0700

    next draft

--- content.js
+++ content.js
@@ -1,16 +1,37 @@
-function ticker() {
+
+$(document).ready(function() {
     let ticker = 'AAPL';
     let tickerregex = new RegExp(ticker, "gi");
-    //"<a href="https://finance.yahoo.com/quote/AAPL">AAPL</a\>"
+        //"<a href="https://finance.yahoo.com/quote/AAPL">AAPL</a\>"
     let replace = "<a href=\"https://finance.yahoo.com/quote/" + ticker + "\">" + ticker +"</a>";
     
-    
-    var strNewString = $('body').html().replace(tickerregex,replace);
-    $('body').html(strNewString);
+    var strNewString = $('.st').html().replace(tickerregex,replace);
+    $('.st').html(strNewString);
+});
 
-    // $(".text_div").text(function () {
-    //     return $(this).text().replace("contains", "hello everyone");
-    //  });
-}
+
+let inputBox = document.getElementById("inputBox");
+inputBox.addEventListener("keydown",function(event){
+    if(event.keyCode === 13) {
+        chrome.tabs.create({url:"https://finance.yahoo.com/quote/"+event.target.value})
+    }
+}) 
+
+let chartBox = document.getElementById("chartButton");
+chartBox.addEventListener("click",function(event){
+    chrome.tabs.create({url:"https://finance.yahoo.com/quote/"+inputBox.value+"/chart"})
+})
+
+let statisticsBox = document.getElementById("statisticsButton");
+statisticsBox.addEventListener("click",function(event){
+    chrome.tabs.create({url:"https://finance.yahoo.com/quote/"+inputBox.value+"/key-statistics"})
+})
+
+let financialsBox = document.getElementById("financialsButton");
+financialsBox.addEventListener("click",function(event){
+    chrome.tabs.create({url:"https://finance.yahoo.com/quote/"+inputBox.value+"/financials"})
+})
+
+
 
 
--- main.js
+++ main.js
@@ -1,44 +1,29 @@
 //$(document).ready(function () {
     //your code here
-    function ticker() {
-        let ticker = 'AAPL';
-        let tickerregex = new RegExp(ticker, "gi");
-        //"<a href="https://finance.yahoo.com/quote/AAPL">AAPL</a\>"
-        let replace = "<a href=\"https://finance.yahoo.com/quote/" + ticker + "\">" + ticker +"</a>";
+    // function ticker() {
+    //     let ticker = 'AAPL';
+    //     let tickerregex = new RegExp(ticker, "gi");
+    //     //"<a href="https://finance.yahoo.com/quote/AAPL">AAPL</a\>"
+    //     let replace = "<a href=\"https://finance.yahoo.com/quote/" + ticker + "\">" + ticker +"</a>";
         
+    //     $("p").each(function(index) {
+    //         let word = 'href';
+    //         $(this).html($(this).text().replace('AAPL', replace));
+            
+    //     })
+    // }
+        //var strNewString = $('body').html().replace(tickerregex,replace);
+        //$('body').html(strNewString);
+        //alert( index + ": " + $(this).text().replace('AAPL', 'GOOG') );
         
-        var strNewString = $('body').html().replace(tickerregex,replace);
-        $('body').html(strNewString);
+        //var strNewString = $('body').html().replace(tickerregex,replace);
+        //$('body').html(strNewString);
 
         // $(".text_div").text(function () {
         //     return $(this).text().replace("contains", "hello everyone");
         //  });
-    }
-//  });
-
-
-
-
-
-// $(document).on('ready', () => {
-//     const title = $('<h1>').text('Stock Ticker');
-//     $('body').append(title);
-//     console.log(title);
-
+    //     })
+    
     
 
-//     $.ajax({
-//         type: 'GET',
-//         url: 'https://finance.yahoo.com/quote/AAPL',
-//         success: function(resp) {
-//         console.log(resp);
-        
-//         //GETS message 
-//         resp.forEach((message, index) => {
-//           let post = $('<div id=' + index + '></div>').html(message.message); 
-//           $('#chatArea').append(post);
-//           })
-//         }
-//       })
-//     })
 
