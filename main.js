686457b7f608d2287e51fe8baacce842a00873a3
Elliot Kim <elliotkim@Elliots-MacBook-Pro.local>
2018-09-19 20:57:12 -0700

    1st draft

---
+++ main.js
@@ -0,0 +1,44 @@
+//$(document).ready(function () {
+    //your code here
+    function ticker() {
+        let ticker = 'AAPL';
+        let tickerregex = new RegExp(ticker, "gi");
+        //"<a href="https://finance.yahoo.com/quote/AAPL">AAPL</a\>"
+        let replace = "<a href=\"https://finance.yahoo.com/quote/" + ticker + "\">" + ticker +"</a>";
+        
+        
+        var strNewString = $('body').html().replace(tickerregex,replace);
+        $('body').html(strNewString);
+
+        // $(".text_div").text(function () {
+        //     return $(this).text().replace("contains", "hello everyone");
+        //  });
+    }
+//  });
+
+const stocks = ['AAPL', 'MSFT'];
+
+
+
+// $(document).on('ready', () => {
+//     const title = $('<h1>').text('Stock Ticker');
+//     $('body').append(title);
+//     console.log(title);
+
+    
+
+//     $.ajax({
+//         type: 'GET',
+//         url: 'https://finance.yahoo.com/quote/AAPL',
+//         success: function(resp) {
+//         console.log(resp);
+        
+//         //GETS message 
+//         resp.forEach((message, index) => {
+//           let post = $('<div id=' + index + '></div>').html(message.message); 
+//           $('#chatArea').append(post);
+//           })
+//         }
+//       })
+//     })
+
